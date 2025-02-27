44ff45d08406857b881fbcf17d3b0c4ce13889f0
shubham <mohanty.shubham007@gmail.com>
2025-02-26 21:37:37 -0500

    feat(meetings): allow students to request meetings & faculty to list their meetings

--- meetings/views.py
+++ meetings/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+# meetings/views.py
 
-# Create your views here.
+from rest_framework import generics, permissions
+from .models import Meeting
+from .serializers import MeetingSerializer
+
+class MeetingListCreateView(generics.ListCreateAPIView):
+    """List all meetings (GET) & allow students to request new meetings (POST)"""
+    queryset = Meeting.objects.all()
+    serializer_class = MeetingSerializer
+
+    def get_queryset(self):
+        """Return meetings relevant to the user"""
+        user = self.request.user
+        if user.role == "faculty":
+            return Meeting.objects.filter(faculty=user)
+        elif user.role == "student":
+            return Meeting.objects.filter(student=user)
+        return Meeting.objects.none()
+
+    def perform_create(self, serializer):
+        """Ensure only students can request meetings"""
+        if self.request.user.role != "student":
+            raise serializers.ValidationError("Only students can request meetings")
+        serializer.save(student=self.request.user)
