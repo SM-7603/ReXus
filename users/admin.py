8ff0b3f134c9f443afb7847ebe0901589790630e
shubham <mohanty.shubham007@gmail.com>
2025-04-03 18:18:43 -0400

    chore(admin): register custom User model with Django admin

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,12 @@
-from django.contrib import admin
+# users/admin.py
 
-# Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    model = User
+    list_display = ("id", "username", "email", "role", "is_staff", "is_superuser", "date_joined")
+    list_filter = ("role", "is_staff")
+    search_fields = ("username", "email")
