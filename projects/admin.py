539efad3ab5339467fdd5cc50b22dffb912d2d0d
shubham <mohanty.shubham007@gmail.com>
2025-04-03 18:19:17 -0400

    chore(admin): register ResearchProject model

--- projects/admin.py
+++ projects/admin.py
@@ -1,3 +1,11 @@
-from django.contrib import admin
+# projects/admin.py
 
-# Register your models here.
+from django.contrib import admin
+from .models import ResearchProject
+
+@admin.register(ResearchProject)
+class ResearchProjectAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "status", "faculty")
+    filter_horizontal = ("students",)
+    search_fields = ("title",)
+    list_filter = ("status",)
