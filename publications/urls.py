e0483cd7c123a26d738ffcadd250d1d09bc2f89e
shubham <mohanty.shubham007@gmail.com>
2025-04-04 01:37:37 -0400

    refactor(publications): separate API and UI routes

--- publications/urls.py
+++ publications/urls.py
@@ -2,8 +2,13 @@
 
 from django.urls import path
 from .views import PublicationListCreateView, PublicationDetailView
+from .views_ui import publications_redirect_view
 
 urlpatterns = [
     path('', PublicationListCreateView.as_view(), name='publication-list-create'),
     path('<int:pk>/', PublicationDetailView.as_view(), name='publication-detail'),
+]
+
+urlpatterns += [
+    path("", publications_redirect_view, name="publications-ui"),
 ]
