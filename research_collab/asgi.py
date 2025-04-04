ee7f08ba2aa7a47134fc530e825ce7f1d573fa13
shubham <mohanty.shubham007@gmail.com>
2025-04-03 22:07:45 -0400

    chore(channels): configure ASGI to support HTTP and WebSocket with AuthMiddleware
    - Loads notifications.routing via ProtocolTypeRouter
    - Ensures correct startup for Channels + Daphne

--- research_collab/asgi.py
+++ research_collab/asgi.py
@@ -1,16 +1,18 @@
-"""
-ASGI config for research_collab project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
-"""
+# research_collab/asgi.py
 
 import os
-
+import django
+from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
+from channels.auth import AuthMiddlewareStack
+from notifications.urls_ws import websocket_urlpatterns # fix
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'research_collab.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "research_collab.settings")
+django.setup()
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(websocket_urlpatterns)
+    ),
+})
