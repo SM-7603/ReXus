8f1a8d443e05dd0f0f812fbf12d91051be1e111a
shubham <mohanty.shubham007@gmail.com>
2025-04-04 01:38:14 -0400

    chore(settings): ensure template dirs are registered

--- research_collab/settings.py
+++ research_collab/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'research_collab.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
