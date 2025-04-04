8926c8202e4a9906d5cea32e2f0f0f9a3a6d0779
shubham <mohanty.shubham007@gmail.com>
2025-04-03 22:06:34 -0400

    feat(notifications): add test API to trigger real-time WebSocket message
    - Simple POST view for sending dummy payload to current user
    - Useful for verifying WebSocket client subscriptions during dev

--- notifications/views.py
+++ notifications/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+# notifications/views.py
 
-# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from .utils import send_realtime_notification
+
+class TestNotificationView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        """Send a test notification to the current user"""
+        payload = {
+            "message": f"Hello {request.user.username}, this is a test notification!",
+            "type": "info"
+        }
+        send_realtime_notification(request.user.id, payload)
+        return Response({"status": "notification sent"})
